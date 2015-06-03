9d605e1177789886af45fe4ee6403f310d467781
Nils Langner <nils.langner@phmlabs.com>
2015-06-03 19:48:31 +0200

    cs

--- src/Http/Request.php
+++ src/Http/Request.php
@@ -1,11 +1,5 @@
 <?php
 
-/**
- * Created by PhpStorm.
- * User: langn
- * Date: 24.05.15
- * Time: 20:18.
- */
 namespace whm\Smoke\Http;
 
 class Request extends \Ivory\HttpAdapter\Message\Request
