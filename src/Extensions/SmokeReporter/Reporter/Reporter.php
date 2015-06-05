fb5f862b9e21094e373d59d01ff67aa6b8a22dc7
Nils Langner <nils.langner@phmlabs.com>
2015-06-05 20:37:40 +0200

    added doc block to reporter interface

--- src/Extensions/SmokeReporter/Reporter/Reporter.php
+++ src/Extensions/SmokeReporter/Reporter/Reporter.php
@@ -4,7 +4,21 @@ namespace whm\Smoke\Extensions\SmokeReporter\Reporter;
 
 interface Reporter
 {
+    /**
+     * This *optional* function can be used to hand over an output interface. This
+     * can be useful if you want to write a cli reporter.
+     */
+    // public function setOutput(OutputInterface $output);
+
+    /**
+     * This function will be called after an url has been validated.
+     *
+     * @param $result the result of the validation
+     */
     public function processResult($result);
 
+    /**
+     * This function is called after the last url has been validated.
+     */
     public function finish();
 }
