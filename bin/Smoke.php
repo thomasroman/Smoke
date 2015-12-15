da0f8ba6d0c6c8c9703fce089973515138e52f23
Nils Langner <contact@phmlabs.com>
2015-12-15 10:33:02 +0100

    Update Smoke.php

--- bin/Smoke.php
+++ bin/Smoke.php
@@ -21,7 +21,7 @@ if (!defined('SMOKE_COMPOSER_INSTALL')) {
 
 $loader = require SMOKE_COMPOSER_INSTALL;
 
-define('SMOKE_VERSION', '1.1.3');
+define('SMOKE_VERSION', '1.2.0');
 
 $app = new \whm\Smoke\Cli\Application();
 $app->run();
