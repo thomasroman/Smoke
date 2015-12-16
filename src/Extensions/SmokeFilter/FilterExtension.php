2e03d00e5624ab15834c34c6523158d9102bc3e0
Nils Langner <langner@plan08.de>
2015-12-16 14:00:30 +0100

    extended filter extiension to handle filter files

--- src/Extensions/SmokeFilter/FilterExtension.php
+++ src/Extensions/SmokeFilter/FilterExtension.php
@@ -3,16 +3,30 @@
 namespace whm\Smoke\Extensions\SmokeFilter;
 
 use phmLabs\Components\Annovent\Event\Event;
-use whm\Smoke\Config\Configuration;
 use whm\Smoke\Http\Response;
+use whm\Smoke\Yaml\EnvAwareYaml;
 
 class FilterExtension
 {
     private $filters = array();
 
-    public function init(Configuration $_configuration, $filters = array())
+    public function init($filters = array(), $filterFile = "")
     {
-        $this->filters = $filters;
+        if ($filterFile != "") {
+            if (!file_exists($filterFile)) {
+                throw new \RuntimeException('Filter file not found: ' . $filterFile);
+            }
+
+            $filterElements = EnvAwareYaml::parse(file_get_contents($filterFile));
+
+            foreach ($filterElements as $rule => $uris) {
+                foreach ($uris as $uri) {
+                    $this->filters[] = array("rule" => $rule, "uri" => $uri);
+                }
+            }
+        } else {
+            $this->filters = $filters;
+        }
     }
 
     /**
@@ -21,9 +35,8 @@ class FilterExtension
     public function isFiltered(Event $event, $ruleName, Response $response)
     {
         foreach ($this->filters as $filter) {
-            if ($ruleName === $filter['rule'] && 0 < preg_match($filter['uri'], (string) $response->getUri())) {
+            if ($ruleName === $filter['rule'] && 0 < preg_match($filter['uri'], (string)$response->getUri())) {
                 $event->setProcessed();
-
                 return true;
             }
         }
