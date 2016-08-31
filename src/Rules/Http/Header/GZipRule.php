5d32271820751eed9fa4806861490f7355e37d9d
Nils Langner <contact@phmlabs.com>
2016-08-31 19:54:22 +0200

    gzip only if not empty document

--- src/Rules/Http/Header/GZipRule.php
+++ src/Rules/Http/Header/GZipRule.php
@@ -16,6 +16,7 @@ class GZipRule extends StandardRule
     {
         if (strpos($response->getContentType(), 'image') === false
             && strpos($response->getContentType(), 'pdf') === false
+            && (string)$response->getBody() != ''
         ) {
             if (!$response->hasHeader('Content-Encoding') || $response->getHeader('Content-Encoding')[0] !== 'gzip') {
                 throw new ValidationFailedException('gzip compression not active');
