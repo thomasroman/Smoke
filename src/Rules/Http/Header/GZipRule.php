39ac743629a3af92f31e1d15b7ace44a7fbc61a1
Nils Langner <contact@phmlabs.com>
2016-10-25 09:00:23 +0200

    gzip compression filter 'minFileSize'

--- src/Rules/Http/Header/GZipRule.php
+++ src/Rules/Http/Header/GZipRule.php
@@ -12,11 +12,18 @@ use whm\Smoke\Rules\ValidationFailedException;
  */
 class GZipRule extends StandardRule
 {
+    private $minFileSize;
+
+    public function init($minFileSize = 200)
+    {
+        $this->minFileSize = $minFileSize;
+    }
+
     public function doValidation(Response $response)
     {
         if (strpos($response->getContentType(), 'image') === false
             && strpos($response->getContentType(), 'pdf') === false
-            && (string) $response->getBody() !== ''
+            && strlen((string)$response->getBody()) >= $this->minFileSize
         ) {
             if (!$response->hasHeader('Content-Encoding') || $response->getHeader('Content-Encoding')[0] !== 'gzip') {
                 throw new ValidationFailedException('gzip compression not active');
