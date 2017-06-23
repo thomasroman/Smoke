2fe123c48b758b83fc95fd4751955d9c184e561b
phmLabs <nils.langner@phmlabs.com>
2017-06-23 16:22:58 +0200

    insecure +exclude

--- src/Rules/Html/InsecureContentRule.php
+++ src/Rules/Html/InsecureContentRule.php
@@ -11,14 +11,23 @@ namespace whm\Smoke\Rules\Html;
 
 use Psr\Http\Message\ResponseInterface;
 use whm\Html\Document;
+use whm\Smoke\Rules\CheckResult;
 use whm\Smoke\Rules\Rule;
-use whm\Smoke\Rules\ValidationFailedException;
 
 /**
  * This rule checks if a https document uses http (insecure) ressources.
  */
 class InsecureContentRule implements Rule
 {
+    private $excludedFiles = [];
+
+    public function init($excludedFiles = [])
+    {
+        foreach ($excludedFiles as $excludedFile) {
+            $this->excludedFiles[] = $excludedFile['file'];
+        }
+    }
+
     public function validate(ResponseInterface $response)
     {
         $uri = $response->getUri();
@@ -31,10 +40,32 @@ class InsecureContentRule implements Rule
 
         $resources = $htmlDocument->getDependencies($uri, false);
 
+        $unsecures = array();
+
         foreach ($resources as $resource) {
             if ($resource->getScheme() && 'https' !== $resource->getScheme()) {
-                throw new ValidationFailedException('At least one dependency was found on a secure url, that was transfered insecure (' . (string)$resource . ')');
+                $excluded = false;
+                foreach ($this->excludedFiles as $excludedFile) {
+                    if (preg_match('*' . $excludedFile . '*', (string)$resource)) {
+                        $excluded = true;
+                        break;
+                    }
+                }
+                if (!$excluded) {
+                    $unsecures[] = $resource;
+                }
             }
+        }
+
+        if (count($unsecures) > 0) {
+            $message = 'At least one dependency was found on a secure url, that was transfered insecure.<ul>';
+            foreach ($unsecures as $unsecure) {
+                $message .= '<li>' . (string)$unsecure . '</li>';
+            }
+            $message .= '</ul>';
+            return new CheckResult(CheckResult::STATUS_FAILURE, $message, count($unsecures));
+        } else {
+            return new CheckResult(CheckResult::STATUS_SUCCESS, 'No http element on that https url found.', 0);
         }
     }
 }
